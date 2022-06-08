97dc864f483074f7b7f7f10946cdccd23f23e7c8
Abhinaysai Kamineni <66816045+askmy-stack@users.noreply.github.com>
2022-06-08 19:43:55 +0530

    Create Array Distinct Numbers.java

---
+++ Array Distinct Numbers.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+class Array Distinct Numbers {
+
+    public static int countDigits(int[] a,int n){
+        int count=0;boolean isDistinct=true;
+        for(int i=0;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(a[i]==a[j]){
+                    isDistinct=false;
+                    break;
+                }
+            }
+            if(isDistinct==true){
+                count++;
+            }
+            isDistinct=true;
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int []a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        System.out.print(countDigits(a,n));
+    }
+}
+
